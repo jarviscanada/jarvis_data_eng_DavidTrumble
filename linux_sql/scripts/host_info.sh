2ec724fca6c7d3a836ca49e072cfa0b3879becdf
David Trumble <dtt.trumble@gmail.com>
2021-09-26 05:45:55 +0000

    ma update

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -11,20 +11,12 @@ psql_user=$4
 psql_password=$5
 
 
-echo $psql_host
-echo $psql_port
-echo $db_name
-echo $psql_user
-echo $psql_password
 
+#of arguments
 echo ' '
-
-echo $1
-echo $2
-echo $3
-echo $4
-echo $5
-
+echo ' '
+echo 'number of arguments'
+echo $#
 echo ' '
 
 
@@ -34,48 +26,48 @@ echo ' '
 #fi
 
 
-#Save machine statistics in MB and current machine hostname to variables
-vmstat_mb=$(vmstat --unit M)
+#lscpu
+lspu_out=$(lscpu)
+
+
+#hostname
+echo "hostname"
 hostname=$(hostname -f)
-
-echo $vmstat_mb
 echo $hostname
-
-#Retrieve hardware specification variables
-#xargs is a trick to trim leading and trailing white spaces
-memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
-echo $memory_free
-
-#cpu_idle=$(echo "$vmstat_mb" | awk '{}' | xargs)
-#echo $cpu_idle
-
-#cpu_kernel=$(echo "$vmstat_mb" ...
-#echo $cpu_kernel
-
-#disk_io=$(vmstat -d | awk '{print $10}' ...
-#echo $disk_io
-
-#disk_available=$(df -BM / ...
-#echo disk_available
+echo ' '
 
 
-#Current time in `2019-11-26 14:40:19` UTC format
-#timestamp=$(vmstat -t | awk ...
-#echo timestamp
-
-#Subquery to find matching id in host_info table
-#host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
-
-
-#insert server usage data into host_usage table
-#insert_stmt="INSERT INTO host_usage(timestamp, ...) VALUES('$timestamp', ..."
+#cpu number
+echo "cpu_number"
+cpu_number=$(echo "$lspu_out" | head -4 | tail -1 | awk '{print $2}')
+echo "$cpu_number"
+echo ' '
 
 
-#set up env var for pql cmd
-#export PGPASSWORD=$psql_password 
+#cpu architect
+echo "cpu_architect"
+cpu_architecture=$(echo "$lspu_out" | head -1 | awk '{print $2}')
+echo "$cpu_architecture"
+echo ' '
+
+echo "cpu_model"
+cpu_model=$(echo "$lscpu_out"  | egrep "^Model" | awk '{print $2}')
+echo "$cpu_model"
+echo ' '
+
+echo "cpu_mhz"
+echo ' '
+
+echo "l2_cache"
+echo ' '
+
+echo "total_mem"
+echo ' '
+
+echo "timestamp"
+echo ' '
 
 
-#Insert date into a database
-#psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+
 
 #exit $?
