4969395d0db35ddac74d4430b6205a6523b40d17
David Trumble <dtt.trumble@gmail.com>
2021-09-27 19:28:37 +0000

    ma update

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -2,6 +2,7 @@
 # File: host_info.sh
 
 
+
 #Setup and validate arguments
 psql_host=$1
 psql_port=$2
@@ -11,41 +12,30 @@ psql_password=$5
 
 
 
-#of arguments
-echo ' '
-echo 'number of arguments'
-echo $#
-
-
 #Check # of args
 #if [ $# -ne 5 ] then 
 #  exit 1
 #fi
 
 
+
 #lscpu
 lscpu_out=$(lscpu)
-
 
 #hostname
 hostname=$(hostname -f)
 
-
 #cpu number
 cpu_number=$(echo "$lscpu_out" | head -4 | tail -1 | awk '{print $2}' | xargs)
-
 
 #cpu architect
 cpu_architecture=$(echo "$lscpu_out" | head -1 | awk '{print $2}' | xargs)
 
-
 #cpu model
 cpu_model=$(echo "$lscpu_out" | egrep "^Model" |  tail -1 | awk '{$1=$2=""; print $0}' | xargs)
 
-
 #cpu mhz
 cpu_mhz=$(echo "$lscpu_out" | tail -10 | head -1 | awk '{print $3}' | xargs)
-
 
 #cpu mhz no K
 cpu_mhz=$(echo "$lscpu_out" | tail -4 | head -1 | awk '{print $3}' | xargs)
@@ -54,17 +44,26 @@ lld_mhz=$(echo "$lscpu_out" | tail -6 | head -1 | awk '{print $3}' | sed 's/.$//
 lli_mhz=$(echo "$lscpu_out" | tail -5 | head -1 | awk '{print $3}' | sed 's/.$//' | xargs) 
 l3_mhz=$(echo "$lscpu_out" | tail -3 | head -1 | awk '{print $3}' | sed 's/.$//' | xargs)
 
-
 #total mem 
 total_mem=$(($l3_mhz + $lli_mhz + $lld_mhz + $lcpu_mhz))
-
 
 #timestamp y/m/d h/m/s - utc
 timestamp=$(date '+%Y/%m/%d %H:%M:%S')
 
 
-exit $?
 
+#insert statement
+insert_stmt="INSERT INTO host_info(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, total_mem, timestamp) VALUES('$hostname', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$toal_mem', '$timestamp'";
+
+
+
+#insert data into database
+#psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+
+
+
+
+exit $?
 
 
 
