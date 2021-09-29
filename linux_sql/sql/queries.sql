1e3d6139b25178ba75a680cbb9bd0c41964faa89
David Trumble <dtt.trumble@gmail.com>
2021-09-29 16:42:29 +0000

    qry complete

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -10,38 +10,21 @@ FROM
 ORDER_BY
 	cpu_number, total_mem DESC;
 
-
-
 -- TODO - AVERAGE MEMORY USAGE
-
--- 	average used memory in in percentage
---	over 5 mins interval for each host
---	user memory = total memory - free memory
-
-
--- 		Sample query output
--- 		host_id, host_name, timestamp,avg_used_mem_percentage
-
--- 		1,node1.jrvs.ca,2019-01-01 00:00:00,97
--- 		1,node1.jrvs.ca,2019-01-01 00:05:00,90
--- 		1,node1.jrvs.ca,2019-01-01 00:10:00,65
--- 		2,node1.jrvs.ca,2019-01-01 00:00:00,50
--- 		2,node1.jrvs.ca,2019-01-01 00:05:00,40
--- 		2,node1.jrvs.ca,2019-01-01 00:10:00,30
-
-
--- SELECT host_id, host_name, timestamp, total_mem- free_mem AS 
--- FROM host_info
-
-
+SELECT 
+	host_id, 
+	(select hostname from 	host_info), 
+	timestamp, 
+	(((select total_mem from host_info) - memory_free) / (select total_mem from host_info)::float *100)::int AS avg_used_mem_percentage 
+FROM 
+	host_usage;
 
 -- TODO DETECT HOST FAILURE
-
--- We can assume that a server is failed when it inserts less than three data points within 5-min interval.
--- Please write a query to detect host failures.
-
-
--- 		host_id, timestamp, num_data_points
---		COUNT DATA POINTS IN 5 MINUTES INTERVALS?
-
---		q is similar to previous one which requires to round timestamp o the nearest 5-minute interval.
+SELECT 
+	host_id, 
+	date_trunc('hour', timestamp) + date_part('minute', timestamp)::int / 5 * interval '5 min' AS timestamp 
+FROM
+	host_usage 
+ORDER BY 
+	timestamp, 
+	host_id;
