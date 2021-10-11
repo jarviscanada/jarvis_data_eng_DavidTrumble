055e9d319033d1823f7516028cb0fed83e401318
David Trumble <dtt.trumble@gmail.com>
2021-10-10 22:38:02 -0400

    jdbc creation

---
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -0,0 +1,9 @@
+package ca.jrvs.apps.jdbc;
+
+public class JDBCExecutor {
+
+    // run all functions
+    public static void main(String[] args) {
+        System.out.println("Hello Learning JDBC");
+    }
+}
