c7de5d1e11087ab96b7978e460d9c8721f24befc
David Trumble <dtt.trumble@gmail.com>
2021-11-10 20:34:30 -0500

    twitter app creation

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/util/TweetUtil.java
@@ -0,0 +1,32 @@
+package ca.jrvs.apps.twitter.util;
+
+import ca.jrvs.apps.twitter.model.Coordinates;
+import ca.jrvs.apps.twitter.model.Tweet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
+public class TweetUtil {
+
+    public static Tweet createTweet(String text,
+                                    double longitude,
+                                    double latitude) {
+
+        final Logger logger = LoggerFactory.getLogger(TweetUtil.class);
+
+        Coordinates geoCoordinates = new Coordinates();
+        Tweet tweet = new Tweet();
+
+        try {
+            geoCoordinates.setCoordinates(Arrays.asList(longitude, latitude));
+            geoCoordinates.setType("Point");
+            tweet.setText(text);
+            tweet.setCoordinates(geoCoordinates);
+        } catch (Exception ex) {
+            logger.info("failed to create tweet from TweetUtil");
+            logger.error(ex.getMessage());
+        }
+        return tweet;
+    }
+}
